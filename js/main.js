4e730d85242247160406961cb9c499d16dcd8b01
Jones Souza <108988209+jonessouzasilva@users.noreply.github.com>
2023-04-01 17:34:42 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
- 
+// Navbar
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+// Remove Menu Icon On Scroll
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+// Header Animation 
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('active', window.scrollY > 0);
+
+});
